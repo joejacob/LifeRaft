324cc2f08d660b29c3bbff3eb9ee3e6b2ab6142b
Sam Ginsburg <sbg11@duke.edu>
2015-11-07 11:26:35 -0500

    initial empty tab view app

---
+++ LifeRaft/LifeRaft/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LifeRaft
+//
+//  Created by Sam Ginsburg on 11/6/15.
+//  Copyright © 2015 sbg11. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
