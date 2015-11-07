8f0f7cdaf94db42265126b59559650d435783316
Sam Ginsburg <sbg11@duke.edu>
2015-11-07 15:55:17 -0500

    set up files for collection view home page

---
+++ LifeRaft/LifeRaft/HomeTitleCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  HomeTitleCollectionReusableView.swift
+//  LifeRaft
+//
+//  Created by Sam Ginsburg on 11/7/15.
+//  Copyright © 2015 sbg11. All rights reserved.
+//
+
+import UIKit
+
+class HomeTitleCollectionReusableView: UICollectionReusableView {
+        
+}
