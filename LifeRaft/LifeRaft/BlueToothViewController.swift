538dc29d4fcc1fe5074a019ed088c31e825d8414
gruxy14 <grant.costa@duke.edu>
2015-11-07 21:26:15 -0500

    new view controllers

--- LifeRaft/LifeRaft/BlueToothViewController.swift
+++ LifeRaft/LifeRaft/BlueToothViewController.swift
@@ -1,25 +1,28 @@
 //
-//  SecondViewController.swift
+//  BlueToothViewController.swift
 //  LifeRaft
 //
-//  Created by Sam Ginsburg on 11/6/15.
+//  Created by Grant Costa on 11/7/15.
 //  Copyright © 2015 sbg11. All rights reserved.
 //
 
+
 import UIKit
+import CoreBluetooth
 
-class SecondViewController: UIViewController {
-
+class BlueToothViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
