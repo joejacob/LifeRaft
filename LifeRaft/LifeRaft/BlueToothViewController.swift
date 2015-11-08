4fceb2aec0ad12245189913280006990df30a277
gruxy14 <grant.costa@duke.edu>
2015-11-07 23:10:38 -0500

    bluetooth connection

--- LifeRaft/LifeRaft/BlueToothViewController.swift
+++ LifeRaft/LifeRaft/BlueToothViewController.swift
@@ -8,21 +8,100 @@
 
 
 import UIKit
-import CoreBluetooth
 
-class BlueToothViewController: UIViewController {
+class ColorSwitchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var connectionsLabel: UILabel!
+    
+    @IBOutlet weak var memberTable: UITableView!
+    
+    var connections = [String]()
+    
+    let colorService = BluetoothManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        colorService.delegate = self
+        memberTable.delegate = self
+        memberTable.dataSource = self
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    /*
+    @IBAction func redTapped(sender: AnyObject) {
+        self.changeColor(UIColor.redColor())
+    }
+    
+    @IBAction func yellowTapped(sender: AnyObject) {
+        self.changeColor(UIColor.yellowColor())
+    }
+    
+    func changeColor(color : UIColor) {
+        UIView.animateWithDuration(0.2) {
+            self.view.backgroundColor = color
+        }
+    }*/
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
     }
     
     
+    func tableView(tableView:UITableView!, numberOfRowsInSection section:Int) -> Int
+    {
+        return connections.count
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        
+            let cell:UITableViewCell=UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "mycell")
+            let newMember = connections[indexPath.row]
+            //let join : UISwitch = UISwitch()
+            //join.frame = CGRectMake(20, 20, 40, 20)
+            
+        
+            
+        
+        
+                cell.textLabel?.text="Name: \(newMember)"
+                
+        
+                //cell.textLabel?.text = "Status: \(uber["status"]!!)"
+                //cell.detailTextLabel?.text="Riders"
+        
+        
+        
+           // print("adding button")
+                return cell
+                //return cell
+    }
+
+ 
 }
 
-
+extension ColorSwitchViewController : BluetoothManagerDelegate {
+    
+    func connectedDevicesChanged(manager: BluetoothManager, connectedDevices: [String]) {
+        NSOperationQueue.mainQueue().addOperationWithBlock { () -> Void in
+            self.connectionsLabel.text = "Connections: \(connectedDevices)"
+            self.connections = connectedDevices
+            self.memberTable.reloadData()
+            
+        }
+    }
+    
+    /*
+    func colorChanged(manager: BluetoothManager, colorString: String) {
+        NSOperationQueue.mainQueue().addOperationWithBlock { () -> Void in
+            switch colorString {
+            case "red":
+                self.changeColor(UIColor.redColor())
+            case "yellow":
+                self.changeColor(UIColor.yellowColor())
+            default:
+                NSLog("%@", "Unknown color value received: \(colorString)")
+            }
+        }
+    }*/
+    
+}
---
+++ LifeRaft/LifeRaft/BluetoothServiceManager.swift
@@ -0,0 +1,124 @@
+//
+//  BluetoothServiceManager.swift
+//  LifeRaft
+//
+//  Created by Grant Costa on 11/7/15.
+//  Copyright © 2015 sbg11. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import MultipeerConnectivity
+
+protocol BluetoothManagerDelegate {
+    
+    func connectedDevicesChanged(manager : BluetoothManager, connectedDevices: [String])
+    //func colorChanged(manager : BluetoothManager, colorString: String)
+    
+}
+
+class BluetoothManager : NSObject {
+    
+    private let BluetoothType = "example-color"
+    private let myPeerId = MCPeerID(displayName: UIDevice.currentDevice().name)
+    private let serviceAdvertiser : MCNearbyServiceAdvertiser
+    private let serviceBrowser : MCNearbyServiceBrowser
+    var delegate : BluetoothManagerDelegate?
+    
+    override init() {
+        self.serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerId, discoveryInfo: nil, serviceType: BluetoothType)
+        
+        self.serviceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: BluetoothType)
+        
+        super.init()
+        
+        self.serviceAdvertiser.delegate = self
+        self.serviceAdvertiser.startAdvertisingPeer()
+        
+        self.serviceBrowser.delegate = self
+        self.serviceBrowser.startBrowsingForPeers()
+    }
+    
+    deinit {
+        self.serviceAdvertiser.stopAdvertisingPeer()
+        self.serviceBrowser.stopBrowsingForPeers()
+    }
+    
+    lazy var session: MCSession = {
+        let session = MCSession(peer: self.myPeerId, securityIdentity: nil, encryptionPreference: MCEncryptionPreference.Required)
+        session.delegate = self
+        return session
+    }()
+}
+
+extension BluetoothManager : MCNearbyServiceAdvertiserDelegate {
+    
+    func advertiser(advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: NSError) {
+        NSLog("%@", "didNotStartAdvertisingPeer: \(error)")
+    }
+    
+    func advertiser(advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: NSData?, invitationHandler: ((Bool, MCSession) -> Void)) {
+        
+        NSLog("%@", "didReceiveInvitationFromPeer \(peerID)")
+        invitationHandler(true, self.session)
+    }
+    
+}
+
+extension BluetoothManager : MCNearbyServiceBrowserDelegate {
+    
+    func browser(browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: NSError) {
+        NSLog("%@", "didNotStartBrowsingForPeers: \(error)")
+    }
+    
+    func browser(browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+        NSLog("%@", "foundPeer: \(peerID)")
+        NSLog("%@", "invitePeer: \(peerID)")
+        browser.invitePeer(peerID, toSession: self.session, withContext: nil, timeout: 10)
+    }
+    
+    func browser(browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        NSLog("%@", "lostPeer: \(peerID)")
+    }
+    
+}
+
+extension MCSessionState {
+    
+    func stringValue() -> String {
+        switch(self) {
+        case .NotConnected: return "NotConnected"
+        case .Connecting: return "Connecting"
+        case .Connected: return "Connected"
+        default: return "Unknown"
+        }
+    }
+    
+}
+
+extension BluetoothManager : MCSessionDelegate {
+    
+    func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
+        NSLog("%@", "peer \(peerID) didChangeState: \(state.stringValue())")
+        self.delegate?.connectedDevicesChanged(self, connectedDevices: session.connectedPeers.map({$0.displayName}))
+    }
+    
+    func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
+        NSLog("%@", "didReceiveData: \(data.length) bytes")
+        let str = NSString(data: data, encoding: NSUTF8StringEncoding) as! String
+        //self.delegate?.colorChanged(self, colorString: str)
+    }
+    
+    func session(session: MCSession, didReceiveStream stream: NSInputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        NSLog("%@", "didReceiveStream")
+    }
+    
+    func session(session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, atURL localURL: NSURL, withError error: NSError?) {
+        NSLog("%@", "didFinishReceivingResourceWithName")
+    }
+    
+    func session(session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, withProgress progress: NSProgress) {
+        NSLog("%@", "didStartReceivingResourceWithName")
+    }
+    
+}
